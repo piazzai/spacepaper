95a8b7e561b4b597834c4b8fed5978490e96d00e
piazzai <42124135+piazzai@users.noreply.github.com>
2024-11-26 22:00:39 +0100

    Start coding demo generation

---
+++ demo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+[[ -z $1 ]] && { echo "error: phone object not provided"; exit 1; }
+[[ -z $2 ]] && { echo "error: tablet object not provided"; exit 1; }
+[[ -z $3 ]] && { echo "error: laptop object not provided"; exit 1; }
+[[ -z $4 ]] && { echo "error: desktop object not provided"; exit 1; }
+
+bash generate-wallpaper.sh 375 812
+bash generate-wallpaper.sh 768 1024
+bash generate-wallpaper.sh 3072 1920
+bash generate-wallpaper.sh 4096 2304
+
+exit
+
+SCRIPTS=$(find ~ -path "*GIMP*scripts")
+
+cp generate-demo.scm "$SCRIPTS"/
+
+gimp -ib "(generate-demo \"$1\" \"demo/mockup-$1.png\")" -b '(gimp-quit 0)'
+
+rm -f "$SCRIPTS"/generate-demo.scm
