7b63032c693d70b55abbfb4aebf55b39353effbf
piazzai <42124135+piazzai@users.noreply.github.com>
2024-11-26 14:08:31 +0100

    Simplify script

--- spacepaper.sh
+++ spacepaper.sh
@@ -3,17 +3,17 @@
 [[ -z $1 ]] && { echo "error: missing object"; exit 1; }
 [[ -z $2 ]] && { echo "error: missing width"; exit 1; }
 [[ -z $3 ]] && { echo "error: missing height"; exit 1; }
-[[ ! -f input/$1.xcf ]] && { echo "error: object not found"; exit 1; }
+[[ ! -f objects/$1.xcf ]] && { echo "error: object not found"; exit 1; }
 
-PAT=$(find ~ -path "*GIMP*/patterns")
-SCM=$(find ~ -path "*GIMP*/scripts")
-OUT=output/"$2"x"$3"
+PATTERNS=$(find ~/.config/GIMP -path "*patterns")
+SCRIPTS=$(find ~/.config/GIMP -path "*scripts")
+OUTPUT="$2"x"$3"
 
-cp film-grain-vertical.pat "$PAT"/
-cp generate-wallpaper.scm "$SCM"/
-mkdir -p "$OUT"
+cp film-grain-vertical.pat "$PATTERNS"/
+cp generate-wallpaper.scm "$SCRIPTS"/
+mkdir -p "$OUTPUT"
 
-gimp -ib "(generate-wallpaper \"$1\" $2 $3 \"$OUT/$1.jpg\")" -b '(gimp-quit 0)'
+gimp -ib "(generate-wallpaper \"$1\" $2 $3 \"$OUTPUT/$1.jpg\")" -b '(gimp-quit 0)'
 
-rm -f "$PATDIR"/film-grain-vertical.pat
-rm -f "$SCMDIR"/generate-wallpaper.scm
+rm -f "$PATTERNS"/film-grain-vertical.pat
+rm -f "$SCRIPTS"/generate-wallpaper.scm
