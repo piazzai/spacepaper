25dd3cfce27fc1dbb174e8ae9139dc776ccd93e1
piazzai <42124135+piazzai@users.noreply.github.com>
2024-11-26 14:38:24 +0100

    Fix filepaths

--- spacepaper.sh
+++ spacepaper.sh
@@ -3,11 +3,11 @@
 [[ -z $1 ]] && { echo "error: missing object"; exit 1; }
 [[ -z $2 ]] && { echo "error: missing width"; exit 1; }
 [[ -z $3 ]] && { echo "error: missing height"; exit 1; }
-[[ ! -f objects/$1.xcf ]] && { echo "error: object not found"; exit 1; }
+[[ ! -f input/$1.xcf ]] && { echo "error: object not found"; exit 1; }
 
 PATTERNS=$(find ~/.config/GIMP -path "*patterns")
 SCRIPTS=$(find ~/.config/GIMP -path "*scripts")
-OUTPUT="$2"x"$3"
+OUTPUT=output/"$2"x"$3"
 
 cp film-grain-vertical.pat "$PATTERNS"/
 cp generate-wallpaper.scm "$SCRIPTS"/
