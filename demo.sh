82b6a8042c00aa7146f7071500284ba54883dd4e
piazzai <42124135+piazzai@users.noreply.github.com>
2024-11-26 22:45:01 +0100

    Finalize demo generation

--- demo.sh
+++ demo.sh
@@ -1,21 +1,25 @@
 #!/bin/bash -e
 
-[[ -z $1 ]] && { echo "error: phone object not provided"; exit 1; }
-[[ -z $2 ]] && { echo "error: tablet object not provided"; exit 1; }
-[[ -z $3 ]] && { echo "error: laptop object not provided"; exit 1; }
-[[ -z $4 ]] && { echo "error: desktop object not provided"; exit 1; }
-
 bash wallpaper.sh 375 812
 bash wallpaper.sh 768 1024
 bash wallpaper.sh 3072 1920
 bash wallpaper.sh 4096 2304
 
-exit
-
 SCRIPTS=$(find ~ -path "*GIMP*scripts")
 
 cp scripts/generate-demo.scm "$SCRIPTS"/
+mkdir -p "demo"
 
-gimp -ib "(generate-demo \"$1\" \"demo/mockup-$1.png\")" -b '(gimp-quit 0)'
+gimp -ib "(generate-demo \"moon\" \"neptune\" \"saturn\" \"venus\" \"$(mktemp -u demo/XXX).png\")" -b '(gimp-quit TRUE)'
+gimp -ib "(generate-demo \"saturn\" \"moon\" \"venus\" \"neptune\" \"$(mktemp -u demo/XXX).png\")" -b '(gimp-quit TRUE)'
+gimp -ib "(generate-demo \"neptune\" \"venus\" \"moon\" \"saturn\" \"$(mktemp -u demo/XXX).png\")" -b '(gimp-quit TRUE)'
+gimp -ib "(generate-demo \"venus\" \"saturn\" \"neptune\" \"moon\" \"$(mktemp -u demo/XXX).png\")" -b '(gimp-quit TRUE)'
+gimp -ib "(generate-demo \"jupiter\" \"uranus\" \"earth\" \"mars\" \"$(mktemp -u demo/XXX).png\")" -b '(gimp-quit TRUE)'
+gimp -ib "(generate-demo \"earth\" \"jupiter\" \"mars\" \"uranus\" \"$(mktemp -u demo/XXX).png\")" -b '(gimp-quit TRUE)'
+gimp -ib "(generate-demo \"uranus\" \"mars\" \"jupiter\" \"earth\" \"$(mktemp -u demo/XXX).png\")" -b '(gimp-quit TRUE)'
+gimp -ib "(generate-demo \"mars\" \"earth\" \"uranus\" \"jupiter\" \"$(mktemp -u demo/XXX).png\")" -b '(gimp-quit TRUE)'
+
+convert -delay 150 -loop 0 -resize 35% demo/*.png demo.gif
 
 rm -f "$SCRIPTS"/generate-demo.scm
+rm -rf "demo/"
