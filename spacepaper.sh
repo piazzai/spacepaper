ab3163fe65d0c273c46612ce746f999ca7ffdc46
piazzai <42124135+piazzai@users.noreply.github.com>
2024-11-26 16:54:44 +0100

    Make object optional

--- spacepaper.sh
+++ spacepaper.sh
@@ -2,19 +2,27 @@
 
 [[ -z $1 ]] && { echo "error: missing width"; exit 1; }
 [[ -z $2 ]] && { echo "error: missing height"; exit 1; }
-[[ -z $3 ]] && { echo "error: missing object"; exit 1; }
-
-[[ ! -f input/$3.xcf ]] && { echo "error: object not found"; exit 1; }
 
 PATTERNS=$(find ~/.config/GIMP -path "*patterns")
 SCRIPTS=$(find ~/.config/GIMP -path "*scripts")
+OBJECTS=$(find input -name "*.xcf" | sed -e "s/.*\/\(.*\).xcf/\1/" | grep -v rings)
 OUTPUT=output/"$1"x"$2"
 
 cp film-grain-vertical.pat "$PATTERNS"/
 cp generate-wallpaper.scm "$SCRIPTS"/
 mkdir -p "$OUTPUT"
 
-gimp -ib "(generate-wallpaper \"$3\" $1 $2 \"$OUTPUT/$3.jpg\")" -b '(gimp-quit 0)'
+if [[ -z $3 ]]; then
+    for OBJECT in $OBJECTS; do
+        gimp -ib "(generate-wallpaper \"$OBJECT\" $1 $2 \"$OUTPUT/$OBJECT.png\")" -b '(gimp-quit 0)'
+        echo "$OBJECT wallpaper saved to ./$OUTPUT/$OBJECT.png"
+    done
+elif [[ -f input/"$3".xcf && $3 != "rings" ]]; then
+    gimp -ib "(generate-wallpaper \"$3\" $1 $2 \"$OUTPUT/$3.png\")" -b '(gimp-quit 0)'
+    echo "$3 wallpaper saved to ./$OUTPUT/$3.png"
+else
+    echo "error: invalid object"
+fi
 
 rm -f "$PATTERNS"/film-grain-vertical.pat
 rm -f "$SCRIPTS"/generate-wallpaper.scm
