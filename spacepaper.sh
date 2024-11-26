b1222ada63f75138709dcdd2f38384c075b7bf3b
piazzai <42124135+piazzai@users.noreply.github.com>
2024-11-26 14:44:58 +0100

    Change order of parameters

--- spacepaper.sh
+++ spacepaper.sh
@@ -1,19 +1,20 @@
 #!/bin/bash -e
 
-[[ -z $1 ]] && { echo "error: missing object"; exit 1; }
-[[ -z $2 ]] && { echo "error: missing width"; exit 1; }
-[[ -z $3 ]] && { echo "error: missing height"; exit 1; }
-[[ ! -f input/$1.xcf ]] && { echo "error: object not found"; exit 1; }
+[[ -z $1 ]] && { echo "error: missing width"; exit 1; }
+[[ -z $2 ]] && { echo "error: missing height"; exit 1; }
+[[ -z $3 ]] && { echo "error: missing object"; exit 1; }
+
+[[ ! -f input/$3.xcf ]] && { echo "error: object not found"; exit 1; }
 
 PATTERNS=$(find ~/.config/GIMP -path "*patterns")
 SCRIPTS=$(find ~/.config/GIMP -path "*scripts")
-OUTPUT=output/"$2"x"$3"
+OUTPUT=output/"$1"x"$2"
 
 cp film-grain-vertical.pat "$PATTERNS"/
 cp generate-wallpaper.scm "$SCRIPTS"/
 mkdir -p "$OUTPUT"
 
-gimp -ib "(generate-wallpaper \"$1\" $2 $3 \"$OUTPUT/$1.jpg\")" -b '(gimp-quit 0)'
+gimp -ib "(generate-wallpaper \"$3\" $1 $2 \"$OUTPUT/$3.jpg\")" -b '(gimp-quit 0)'
 
 rm -f "$PATTERNS"/film-grain-vertical.pat
 rm -f "$SCRIPTS"/generate-wallpaper.scm
