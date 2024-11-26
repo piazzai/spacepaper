b6d23ebc1b0d8fe477114bd1dbac07a0df72ee10
piazzai <42124135+piazzai@users.noreply.github.com>
2024-11-26 23:09:52 +0100

    Rename output folder

--- wallpaper.sh
+++ wallpaper.sh
@@ -6,7 +6,7 @@
 PATTERNS=$(find ~ -path "*GIMP*patterns")
 SCRIPTS=$(find ~ -path "*GIMP*scripts")
 OBJECTS=$(find gimp -name "*.xcf" | sed -e "s/.*\/\(.*\).xcf/\1/" | grep -v rings | grep -v mockup)
-OUTPUT=output/"$1"x"$2"
+OUTPUT=wallpaper/"$1"x"$2"
 
 cp gimp/patterns/film-grain-vertical.pat "$PATTERNS"/
 cp gimp/scripts/generate-wallpaper.scm "$SCRIPTS"/
@@ -15,12 +15,12 @@ if [[ -z $3 ]]; then
     mkdir -p "$OUTPUT"
     for OBJECT in $OBJECTS; do
         gimp -ib "(generate-wallpaper \"$OBJECT\" $1 $2 \"$OUTPUT/$OBJECT.png\")" -b '(gimp-quit TRUE)'
-        echo "$OBJECT wallpaper saved to ./$OUTPUT/$OBJECT.png"
+        echo "wallpaper saved to ./$OUTPUT/$OBJECT.png"
     done
 elif [[ -f gimp/"$3".xcf && $3 != "rings" && $3 != "mockup" ]]; then
     mkdir -p "$OUTPUT"
     gimp -ib "(generate-wallpaper \"$3\" $1 $2 \"$OUTPUT/$3.png\")" -b '(gimp-quit TRUE)'
-    echo "$3 wallpaper saved to ./$OUTPUT/$3.png"
+    echo "wallpaper saved to ./$OUTPUT/$3.png"
 else
     echo "error: object not valid"
 fi
